d6ce34a5027e162a816f76da8c02722951cb6031
Daniel <danieldavydov0@gmail.com>
2025-04-18 16:21:17 -0400

    add: validation for url and alias; add: functionality to create alias

--- components/Container.tsx
+++ components/Container.tsx
@@ -2,6 +2,7 @@
 
 import Link from "next/link";
 import { useState } from "react";
+import getCollection, { ALIAS_COLLECTION } from "@/db";
 
 export default function Container() {
   const [url, setUrl] = useState("");
